9af5ff54c5a60a78cbcfbfd385e62d4bdbabd279
abhijeet1984 <abhijeet.chaudhury@gmail.com>
2013-09-19 18:25:50 +0530

    junit

---
+++ src/com/learning/wipro/MultiplyClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.learning.wipro;
+
+/**
+ * @author AB277176
+ *
+ */
+public class MultiplyClass {
+	
+	public int multiply(int x,int y){
+		System.out.println("Multipication of "+x+" &"+y+" is"+x*y);
+		return x*y;
+	}
+	
+	public int divison(int x,int y){
+		System.out.println("Division of "+x+" &"+y+" is"+x/y);
+		return x/y;
+	}
+
+}
---
+++ src/com/testing/wipro/MultiplyClassTest.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.testing.wipro;
+
+import static org.junit.Assert.*;
+
+import org.jboss.util.loading.MyClassLoaderClassLoaderSource;
+import org.junit.Before;
+import org.junit.Test;
+import com.learning.wipro.*;
+
+/**
+ * @author AB277176
+ *
+ */
+public class MultiplyClassTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("System Set Up");
+	}
+
+	/**
+	 * Test method for {@link com.learning.wipro.MultiplyClass#multiply(int, int)}.
+	 */
+	@Test
+	public void testMultiply() {
+		MultiplyClass multi = new MultiplyClass();
+		int test =multi.multiply(5,7);
+		//assertTrue(test==35);
+		//assertTrue("Success", test==35);
+		assertTrue("Suc", test!=45);
+		//fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	public void testDivison() {
+		MultiplyClass multi = new MultiplyClass();
+		int test =multi.divison(35,7);
+		//assertTrue(test==35);
+		//assertTrue("Success", test==35);
+		assertTrue("Suc", test!=5);
+		//fail("Not yet implemented");
+		
+	}
+
+}
